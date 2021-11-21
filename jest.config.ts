60441a0072e157e8945c8f4c366b1b326db43ee2
Jean-Philippe Monette <contact@jpmonette.net>
2021-11-21 16:07:29 +0000

    Add Coveralls

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "__tests__/util/"],
+  testRegex: "(/__tests__/.*\\.test)\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
