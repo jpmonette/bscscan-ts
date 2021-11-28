d5a1fc1e950d821edba5d675b7edd37ded76100b
Jean-Philippe Monette <contact@jpmonette.net>
2021-11-28 01:02:31 +0000

    Ignore tests in lib/

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,6 @@ export default {
   testEnvironment: "node",
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "!**/*.d.ts", "!src/typings/**", "!src/__tests__/**"],
   testPathIgnorePatterns: ["/node_modules/"],
-  testRegex: "(/__tests__/.*\\.test)\\.(jsx?|tsx?)$",
+  testRegex: "(/__tests__/.*\\.test)\\.(tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
